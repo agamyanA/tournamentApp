4566b7e5e3392533984f755a375fa1153f6f142f
Alex <agamyan@outlook.com>
2021-05-04 21:41:42 +0400

    ....

--- app/app.js
+++ app/app.js
@@ -16,6 +16,10 @@ class Match {
         this.teamOne = teamOne
         this.teamTwo = teamTwo
     }
+
+    get winner() {
+        return this.teamOne.score > this.teamTwo.score ? this.teamOne.name : this.teamTwo.name
+    }
 }
 
 async function getData() {
@@ -24,21 +28,22 @@ async function getData() {
     return Object.values(data).map(teams => teams.team_name)
 }
 
-async function aaa() {
+async function randomMatchMaker() {
     const teams = await getData()
     let filteredTeams = [...teams]
-    let left = Array.from(roundOfSixteen).filter((elem, index) => index % 2 === 0 )
-    let rigth = Array.from(roundOfSixteen).filter((elem, index) => index % 2 !== 0 )
+    let sideL = Array.from(roundOfSixteen).filter((elem, index) => index % 2 === 0)
+    let sideR = Array.from(roundOfSixteen).filter((elem, index) => index % 2 !== 0)
     let matches = []
-console.log(left);
-console.log(rigth);
-    for (let i = 0; i < teams.length/2; i++) {
+
+    for (let i = 0; i < teams.length / 2; i++) {
+
         let teamOne = filteredTeams[Math.floor(Math.random() * filteredTeams.length)]
-        left[i].textContent = teamOne
         filteredTeams = filteredTeams.filter(teamName => teamName !== teamOne)
+        sideL[i].textContent = teamOne
+
         let teamTwo = filteredTeams[Math.floor(Math.random() * filteredTeams.length)]
-        rigth[i].textContent = teamTwo
         filteredTeams = filteredTeams.filter(teamName => teamName !== teamTwo)
+        sideR[i].textContent = teamTwo
         
         matches.push(
             new Match(
@@ -47,8 +52,57 @@ console.log(rigth);
             )
         )
     }
-    
-    console.log(matches[1].teamOne.name);
+
+    return matches
 }
 
-aaa()
+async function matchMaker(matchWinners) {
+
+    let match = []
+    let teams = await matchWinners
+
+    for (let i = 0; i < teams.length; i += 2) {
+        match.push(
+            new Match(
+                new Team(teams[i], Math.floor(Math.random() * 5)),
+                new Team(teams[i + 1], Math.floor(Math.random() * 5))
+            )
+        )
+    }
+    return match
+}
+
+async function winners(match) {
+    
+        const matches = await match
+        let winners = []
+    
+        for (let i = 0; i < matches.length; i++) {
+            winners.push(matches[i].winner)
+        }
+
+        return winners
+}
+
+async function render(round, winners) {
+
+    let winner = await winners
+
+    round.forEach((elem, i) => elem.textContent = winner[i]);
+}
+
+// let a = winners(randomMatchMaker())
+
+// render(roundOfEight, a)
+
+// let b = matchMaker(a)
+
+// let q = winners(b)
+
+// render(semiFinal, q)
+
+// let d = matchMaker(q)
+
+// let u = winners(d)
+
+// render(final, u)
