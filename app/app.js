71a7d945e167056bc07068989c8466bd7fdcbc11
Alex <agamyan@outlook.com>
2021-05-06 15:28:00 +0400

    the end

--- app/app.js
+++ app/app.js
@@ -4,6 +4,7 @@ const roundOfEight = document.querySelectorAll('[data-match="roundOfEight"]')
 const semiFinal = document.querySelectorAll('[data-match="semiFinal"]')
 const final = document.querySelectorAll('[data-match="final"]')
 const allTeams = document.querySelectorAll('.team__name')
+const winner = document.querySelector('#winner')
 const start = document.querySelector('#start')
 const reset = document.querySelector('#reset')
 
@@ -30,15 +31,11 @@ class Tournament {
     static #matches
     static #winners
     static #scores
+    static #index = 0
     static #round = [roundOfEight, semiFinal, final]
 
-    static async #getData() {
-        const response = await fetch(`https://${DATABASE}.firebasedatabase.app/data.json`)
-        const data = await response.json()
-        return Object.values(data).map(teams => teams.team_name)
-    }
-
     static #generator(matches, teamOne, teamTwo) {
+
         matches.push(
             new Match(
                 new Team(teamOne, Math.floor(Math.random() * 5)),
@@ -47,13 +44,21 @@ class Tournament {
         )
     }
 
+    static async #getData() {
+
+        const response = await fetch(`https://${DATABASE}.firebasedatabase.app/data.json`)
+        const data = await response.json()
+        return Object.values(data).map(teams => teams.team_name)
+    }
+
     static async #randomMatchMaker() {
+
         const teams = await this.#getData()
         let filteredTeams = [...teams]
         let sideL = Array.from(roundOfSixteen).filter((elem, index) => index % 2 === 0)
         let sideR = Array.from(roundOfSixteen).filter((elem, index) => index % 2 !== 0)
         let matches = []
-
+        // creating random matches
         for (let i = 0; i < teams.length / 2; i++) {
 
             let teamOne = filteredTeams[Math.floor(Math.random() * filteredTeams.length)]
@@ -64,16 +69,19 @@ class Tournament {
 
             this.#generator(matches, teamOne, teamTwo)
         }
-
+        // rendering matches
         for (let i = 0; i < teams.length / 2; i++) {
-            sideL[i].textContent = matches[i].teamOne.name + matches[i].teamOne.score
-            sideR[i].textContent = matches[i].teamTwo.name + matches[i].teamTwo.score
+            sideL[i].innerHTML = matches[i].teamOne.name +
+                                `<span class="team__score">${matches[i].teamOne.score}</span>`
+            sideR[i].innerHTML = matches[i].teamTwo.name +
+                                `<span class="team__score">${matches[i].teamTwo.score}</span>`
         }
 
         return matches
     }
 
     static #matchMaker() {
+
         let teams = this.#winners
         let matches = []
 
@@ -85,6 +93,7 @@ class Tournament {
     }
 
     static #winner() {
+
         let matches = this.#matches
         let winners = []
 
@@ -96,6 +105,7 @@ class Tournament {
     }
 
     static #score() {
+
         let matches = this.#matches
         let scores = []
 
@@ -108,35 +118,42 @@ class Tournament {
     }
 
     static #render(round) {
+
         let teamName = this.#winners
         let teamScore = this.#scores
 
-        round.forEach((elem, i) => elem.textContent = teamName[i] + teamScore[i]);
+        round.forEach((elem, i) => {
+            elem.innerHTML = teamName[i] + `<span class="team__score">${teamScore[i]}</span>`
+        });
+
+        if (this.#matches.length === 1) {
+            winner.textContent = `Winner is ${this.#matches[0].winner.name}`
+        }
     }
-
+    
     static async start() {
-        this.#matches = await this.#randomMatchMaker()
-        this.#winners =  this.#winner()
-        this.#matches = this.#matchMaker()
-        this.#scores = this.#score()
-        this.#render(this.#round[0])
 
-        this.#winners =  this.#winner()
-        this.#matches = this.#matchMaker()
-        this.#scores = this.#score()
+        if (this.#matches === undefined) {
 
-        this.#render(this.#round[1])
-        this.#winners = this.#winner()
-        this.#matches = this.#matchMaker()
-        this.#scores = this.#score()
-        this.#render(this.#round[2])
-
+            this.#matches = await this.#randomMatchMaker()
+            this.#winners =  this.#winner()
+        } else {
+            
+            this.#winners =  this.#winner()
+            this.#matches = this.#matchMaker()
+            this.#scores = this.#score()
+            this.#render(this.#round[this.#index])
+            this.#index++
+        }
     }
     
     static reset() {
+        this.#index = 0
+        this.#matches = undefined
         allTeams.forEach(elem => elem.textContent = ' ')
+        winner.textContent = ''
     }
 }
 
 start.addEventListener('click', Tournament.start.bind(Tournament))
-reset.addEventListener('click', Tournament.reset)
+reset.addEventListener('click', Tournament.reset.bind(Tournament))
